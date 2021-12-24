d89ec537e115024a2d249e89406e05b326468c2c
Erik Reider <ereider20010103@gmail.com>
2021-12-24 21:52:20 +0100

    Added fish shell completions

---
+++ completions/fish/swaync.fish
@@ -0,0 +1,4 @@
+complete -f -c swaync
+complete -c swaync -s h -l help --description "Show help options"
+complete -c swaync -s s -l style --description "Use a custom Stylesheet file" -r
+complete -c swaync -s c -l config --description "Use a custom config file" -r

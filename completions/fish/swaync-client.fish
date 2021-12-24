e85c2f5d6d9010a66550e0c9f094ec9d76733b2c
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2021-12-24 21:53:28 +0100

    Terminal completions (#44)
    * Added ZSH completions
    
    * Fixed single quote ending string
    
    * Added Bash completions
    
    * Added bash to meson.build
    
    * Added bash variable to meson_options.txt
    
    * Added fish shell completions

---
+++ completions/fish/swaync-client.fish
@@ -0,0 +1,11 @@
+complete -f -c swaync-client
+complete -c swaync-client -s h -l help --description "Show help options"
+complete -c swaync-client -s R -l reload-config --description "Reload the config file" -r
+complete -c swaync-client -s rs -l reload-css --description "Reload the css file. Location change requires restart" -r
+complete -c swaync-client -s t -l toggle-panel --description "Toggle the notificaion panel" -r
+complete -c swaync-client -s d -l toggle-dnd --description "Toggle and print the current dnd state" -r
+complete -c swaync-client -s D -l get-dnd --description "Print the current dnd state" -r
+complete -c swaync-client -s c -l count --description "Print the current notificaion count" -r
+complete -c swaync-client -s C -l close-all --description "Closes all notifications" -r
+complete -c swaync-client -s sw -l skip-wait --description "Doesn't wait when swaync hasn't been started" -r
+complete -c swaync-client -s s -l subscribe --description "Subscribe to notificaion add and close events" -r

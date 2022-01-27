6e57284bc25de01be5deef84a3f52bd233b010d4
Erik Reider <ereider20010103@gmail.com>
2022-01-27 18:26:05 +0100

    Updated shell completions

--- completions/fish/swaync.fish
+++ completions/fish/swaync.fish
@@ -1,4 +1,5 @@
 complete -f -c swaync
 complete -c swaync -s h -l help --description "Show help options"
+complete -c swaync -s v -l version --description "Prints version"
 complete -c swaync -s s -l style --description "Use a custom Stylesheet file" -r
 complete -c swaync -s c -l config --description "Use a custom config file" -r

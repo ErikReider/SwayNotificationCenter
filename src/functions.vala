ErikReider/SwayNotificationCenter

commit dc7cd1afaa78afb3976653e0b4a13173ed331e83

Run script commands with "/bin/sh -c ..." (#519)

src/functions.vala
@@ -357,8 +357,8 @@ namespace SwayNotificationCenter {
                     spawn_env += additions;
                 }
 
-                string[] argvp = {};
-                Shell.parse_argv (cmd, out argvp);
+                string[] argvp;
+                Shell.parse_argv ("/bin/sh -c %s".printf (cmd), out argvp);
 
                 Pid child_pid;
                 int std_output;

commit d0998d02c4766930565bc23e8e59bc1e110a4302

Fixed not being able to run multiple commands regression by #519
Thanks @jbeich for finding this bug

src/functions.vala
@@ -358,7 +358,7 @@ namespace SwayNotificationCenter {
                 }
 
                 string[] argvp;
-                Shell.parse_argv ("/bin/sh -c %s".printf (cmd), out argvp);
+                Shell.parse_argv ("/bin/sh -c \"%s\"".printf (cmd), out argvp);
 
                 Pid child_pid;
                 int std_output;
@@ -381,13 +381,17 @@ namespace SwayNotificationCenter {
                         return false;
                     }
                     try {
-                        channel.read_line (out res, null, null);
+                        if (channel.read_line (out res, null, null) == IOStatus.NORMAL) {
+                            debug ("Exec output:\n%s", res);
+                        } else {
+                            res = "";
+                        }
                         return true;
                     } catch (IOChannelError e) {
-                        stderr.printf ("stdout: IOChannelError: %s\n", e.message);
+                        warning ("stdout: IOChannelError: %s", e.message);
                         return false;
                     } catch (ConvertError e) {
-                        stderr.printf ("stdout: ConvertError: %s\n", e.message);
+                        warning ("stdout: ConvertError: %s", e.message);
                         return false;
                     }
                 });
@@ -400,12 +404,12 @@ namespace SwayNotificationCenter {
                     end_status = status;
                     execute_command.callback ();
                 });
-                // Waits until `run_script.callback()` is called above
+                // Waits until `execute_command.callback()` is called above
                 yield;
                 msg = res;
                 return end_status == 0;
             } catch (Error e) {
-                stderr.printf ("Run_Script Error: %s\n", e.message);
+                warning ("Execute Command Error: %s", e.message);
                 msg = e.message;
                 return false;
             }